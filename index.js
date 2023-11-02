9ef5df2d7ddc175f42472c5fadbf34e0e1456ca8
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-02 01:38:53 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// write code here 
+let num1 = Number.parseInt("31", 10);
+let num2 = Number.parseInt("2", 10);
 
-// Write your code here
+const multiply = (num1 * num2)
+
+console.log(multiply)
+
+
+const random = Math.floor(Math.random() * 10 + 1)
+
+console.log(random)
+
+let num3 =  Number.parseInt("4", 10);
+let num4 =  Number.parseInt("16", 10);
+
+const mod = num4 / num3
+
+console.log(mod)
+
+const max = Math.max(14, 16, 18, 20); 
+
+console.log(max)
+
+
+
+
